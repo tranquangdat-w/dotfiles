2a45d4280f748656c7e477640970a2f777c447af
dat <tranquangdat23122004@gmail.com>
2025-03-28 11:16:35 +0700

    first commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,6 +64,7 @@ vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 vim.keymap.set("n", "<Leader>1", function() harpoon:list():select(1) end, {desc = 'Go to harpoon 1'})
 vim.keymap.set("n", "<Leader>2", function() harpoon:list():select(2) end, {desc = 'Go to harpoon 2'})
 vim.keymap.set("n", "<Leader>3", function() harpoon:list():select(3) end, {desc = 'Go to harpoon 3'})
+vim.keymap.set("n", "<Leader>4", function() harpoon:list():select(4) end, {desc = 'Go to harpoon 4'})
 vim.keymap.set("n", "<C-w>h", "<C-w>v")
 vim.keymap.set("n", "<C-w>v", "<C-w>s")
 
