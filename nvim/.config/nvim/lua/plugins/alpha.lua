f3bda253112ac820c70812b379559e401a99e48c
tran quang dat <tranquangdat23122004@gmail.com>
2025-08-02 00:29:19 +0700

    update

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -1,6 +1,15 @@
 return {
-    'goolord/alpha-nvim',
-    config = function ()
-        require'alpha'.setup(require'alpha.themes.dashboard'.config)
-    end
+  'goolord/alpha-nvim',
+  config = function()
+    local dashboard = require 'alpha.themes.dashboard'
+    dashboard.section.buttons.val = {
+      dashboard.button("SPC f f", "  Find File"),
+      dashboard.button("SPC f g", "󰱽  Live Grep"),
+      dashboard.button("SPC f G", "󰱽  Live Grep (hidden files)"),
+      dashboard.button("SPC f b", "  Buffers"),
+      dashboard.button("SPC f s", "  Grep with input"),
+      dashboard.button("SPC f w", "󰈬  Grep in Current Buffer"),
+    }
+    require 'alpha'.setup(dashboard.config)
+  end
 };
---
+++ nvim/.config/nvim/lua/plugins/support-cmd.lua
@@ -0,0 +1,18 @@
+return {
+  'gelguy/wilder.nvim',
+  config = function()
+    local wilder = require('wilder')
+    wilder.setup(
+      {
+        modes = { ':', '/', '?' },
+        next_key = "<C-j>",
+        previous_key = "<C-k>"
+      }
+    )
+    wilder.set_option('renderer', wilder.popupmenu_renderer({
+      highlighter = wilder.basic_highlighter(),
+      left = { ' ', wilder.popupmenu_devicons() },
+      right = { ' ', wilder.popupmenu_scrollbar() },
+    }))
+  end
+}
