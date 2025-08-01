d0f2b180b7624b2807d299acdeb6e7174edfc29a
tran quang dat <tranquangdat23122004@gmail.com>
2025-08-01 22:04:45 +0700

    change function

--- nvim/.config/nvim/lua/plugins/find-function.lua
+++ nvim/.config/nvim/lua/plugins/find-function.lua
@@ -70,6 +70,9 @@ return {
     vim.keymap.set("n", "<leader>fh", function() toggle_telescope(harpoon:list()) end,
       { desc = "Open harpoon window" })
 
+    vim.keymap.set("n", "<leader>nf", ":AerialNext<CR>", { desc = "Move to next function" })
+    vim.keymap.set("n", "<leader>nf", ":AerialPrev<CR>", { desc = "Move to previous function" })
+
     -- vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find files" })
     -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Live grep" })
     -- vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffers" })
