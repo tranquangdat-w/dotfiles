91eba3da9199f931c81b1a6cc756c41b6627b70e
tran quang dat <tranquangdat23122004@gmail.com>
2025-08-02 12:36:18 +0700

    update

--- nvim/.config/nvim/lua/plugins/alpha.lua
+++ nvim/.config/nvim/lua/plugins/alpha.lua
@@ -3,9 +3,9 @@ return {
   config = function()
     local dashboard = require 'alpha.themes.dashboard'
     dashboard.section.buttons.val = {
+      dashboard.button("SPC v v", "  Open Oil"),
       dashboard.button("SPC f f", "  Find File"),
       dashboard.button("SPC f g", "󰱽  Live Grep"),
-      dashboard.button("SPC f G", "󰱽  Live Grep (hidden files)"),
       dashboard.button("SPC f b", "  Buffers"),
       dashboard.button("SPC f s", "  Grep with input"),
       dashboard.button("SPC f w", "󰈬  Grep in Current Buffer"),
