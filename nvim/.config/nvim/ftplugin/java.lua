2abaea351e97d76ecd4e50254fa775db1d4a51b3
tran quang dat <tranquangdat23122004@gmail.com>
2025-08-02 14:41:15 +0700

    update

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -5,14 +5,15 @@ local workspace_dir = workspace_path .. project_name
 
 local status, jdtls = pcall(require, 'jdtls')
 if not status then
-    return
+  return
 end
 
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
 
 -- CHECK
 local bundles = {
-  vim.fn.glob('~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar')
+  vim.fn.glob(
+    '~/.local/share/nvim/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar')
 }
 
 local on_attach = function(client, bufnr)
@@ -21,66 +22,65 @@ end
 
 
 local config = {
-    cmd = {
-        'java',
-        '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-        '-Dosgi.bundles.defaultStartLevel=4',
-        '-Declipse.product=org.eclipse.jdt.ls.core.product',
-        '-Dlog.protocol=true',
-        '-Dlog.level=ALL',
-        '-Xmx1g',
-        '--add-modules=ALL-SYSTEM',
-        '--add-opens',
-        'java.base/java.util=ALL-UNNAMED',
-        '--add-opens',
-        'java.base/java.lang=ALL-UNNAMED',
-        '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
-        '-jar',
-        vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-        '-configuration',
-        home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
-        '-data',
-        workspace_dir,
-    },
-    root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
-    on_attach = on_attach,
+  cmd = {
+    'java',
+    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+    '-Dosgi.bundles.defaultStartLevel=4',
+    '-Declipse.product=org.eclipse.jdt.ls.core.product',
+    '-Dlog.protocol=true',
+    '-Dlog.level=ALL',
+    '-Xmx1g',
+    '--add-modules=ALL-SYSTEM',
+    '--add-opens',
+    'java.base/java.util=ALL-UNNAMED',
+    '--add-opens',
+    'java.base/java.lang=ALL-UNNAMED',
+    '-javaagent:' .. home .. '/.local/share/nvim/mason/packages/jdtls/lombok.jar',
+    '-jar',
+    vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+    '-configuration',
+    home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+    '-data',
+    workspace_dir,
+  },
+  root_dir = require('jdtls.setup').find_root { '.git', 'mvnw', 'gradlew', 'pom.xml', 'build.gradle' },
+  on_attach = on_attach,
 
-    settings = {
-        java = {
-            signatureHelp = { enabled = true },
-            extendedClientCapabilities = extendedClientCapabilities,
-            maven = {
-                downloadSources = true,
-            },
-            referencesCodeLens = {
-                enabled = true,
-            },
-            references = {
-                includeDecompiledSources = true,
-            },
-            inlayHints = {
-                parameterNames = {
-                    enabled = 'all', -- literals, all, none
-                },
-            },
-            format = {
-                enabled = false,
-            },
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      extendedClientCapabilities = extendedClientCapabilities,
+      maven = {
+        downloadSources = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = 'all', -- literals, all, none
         },
+      },
+      format = {
+        enabled = true,
+      },
     },
+  },
 
-    init_options = {
-        bundles = bundles,
-    },
+  init_options = {
+    bundles = bundles,
+  },
 }
 require('jdtls').start_or_attach(config)
 
 vim.keymap.set('n', '<leader>co', "<Cmd>lua require'jdtls'.organize_imports()<CR>", { desc = 'Organize Imports' })
 vim.keymap.set('n', '<leader>crv', "<Cmd>lua require('jdtls').extract_variable()<CR>", { desc = 'Extract Variable' })
 vim.keymap.set('v', '<leader>crv', "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-    { desc = 'Extract Variable' })
+  { desc = 'Extract Variable' })
 vim.keymap.set('n', '<leader>crc', "<Cmd>lua require('jdtls').extract_constant()<CR>", { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crc', "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-    { desc = 'Extract Constant' })
+  { desc = 'Extract Constant' })
 vim.keymap.set('v', '<leader>crm', "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", { desc = 'Extract Method' })
-
--- nvim/.config/nvim/lua/plugins/lsp-configs.lua
+++ nvim/.config/nvim/lua/plugins/lsp-configs.lua
@@ -37,24 +37,17 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
 
-      -- Auto format code on save
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
-
-          ---@diagnostic disable-next-line: missing-parameter
-          if client.supports_method('textDocument/formatting') then
-            --- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              buffer = args.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-              end
-            })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        callback = function()
+          local mode = vim.api.nvim_get_mode().mode
+          local filetype = vim.bo.filetype
+          if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
+            vim.cmd('lua vim.lsp.buf.format()')
+          else
           end
         end
       })
+
       -- lua
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
