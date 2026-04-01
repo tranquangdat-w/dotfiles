b14cad489f1f754ba404ae1e1623f67a78aad1a9
Tran Quang Dat(NES) <dat.tran5@ntq-solution.com.vn>
2026-04-01 13:43:00 +0700

    update the scroll for best

--- nvim/.config/nvim/lua/plugins/scroll.lua
+++ nvim/.config/nvim/lua/plugins/scroll.lua
@@ -10,7 +10,7 @@ return {
       respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
       cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
       duration_multiplier = 0.8,   -- Global duration multiplier
-      easing = "linear",           -- Default easing function
+      easing = "quadratic",           -- Default easing function
       pre_hook = nil,              -- Function to run before the scrolling animation starts
       post_hook = nil,             -- Function to run after the scrolling animation ends
       performance_mode = false,    -- Disable "Performance Mode" on all buffers.
