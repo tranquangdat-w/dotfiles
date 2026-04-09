d87f4b0d8b7c8be97dafe42d40f30da102f84ea7
Tran Quang Dat(NES) <dat.tran5@ntq-solution.com.vn>
2026-04-09 14:02:58 +0700

    update opencod

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -1,23 +1,16 @@
 return {
   "nickjvandyke/opencode.nvim",
-  version = "*", -- Latest stable release
+  version = "*",
   dependencies = {
     {
-      -- `snacks.nvim` integration is recommended, but optional
-      ---@module "snacks" <- Loads `snacks.nvim` types for configuration intellisense
       "folke/snacks.nvim",
-      optional = true,
       opts = {
-        input = {}, -- Enhances `ask()`
-        picker = { -- Enhances `select()`
-          actions = {
-            opencode_send = function(...) return require("opencode").snacks_picker_send(...) end,
-          },
-          win = {
-            input = {
-              keys = {
-                ["<a-a>"] = { "opencode_send", mode = { "n", "i" } },
-              },
+        input = { enabled = true },
+        styles = {
+          input = {
+            keys = {
+              i_ctrl_j = "<C-n>",
+              i_ctrl_k = "<C-p>",
             },
           },
         },
@@ -25,18 +18,9 @@ return {
     },
   },
   config = function()
-    ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      -- Your configuration, if any; goto definition on the type or field for details
-    }
-
-    vim.o.autoread = true -- Required for `opts.events.reload`
-
-    -- Recommended/example keymaps
+    vim.o.autoread = true
     vim.keymap.set({ "n", "x" }, "<leader>o", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
     vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
     vim.keymap.set({ "n", "x" }, "go",  function() return require("opencode").operator("@this ") end,        { desc = "Add range to opencode", expr = true })
-    vim.keymap.set("n",          "goo", function() return require("opencode").operator("@this ") .. "_" end, { desc = "Add line to opencode", expr = true })
-
   end,
 }
