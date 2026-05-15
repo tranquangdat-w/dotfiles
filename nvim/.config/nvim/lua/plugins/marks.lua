1e398a8559e009862acb623dfe81a63cde3ea96e
Tran Quang Dat(NES) <dat.tran5@ntq-solution.com.vn>
2026-05-15 10:20:24 +0700

    updatemark

--- nvim/.config/nvim/lua/plugins/marks.lua
+++ nvim/.config/nvim/lua/plugins/marks.lua
@@ -1,8 +1,47 @@
 return {
   "chentoast/marks.nvim",
   event = "VeryLazy",
+
   opts = {},
+
   keys = {
     { "<leader>`", "<cmd>MarksQFListAll<cr>", desc = "Open quickfix all marks" },
   },
+
+  config = function(_, opts)
+    require("marks").setup(opts)
+
+    local function set_mark(mark)
+      local pos = vim.api.nvim_buf_get_mark(0, mark)
+
+      if pos[1] ~= 0 then
+        vim.api.nvim_echo({
+          {
+            ("Overwrite mark '%s' at line %d? [y/N]: ")
+              :format(mark, pos[1]),
+            "Question",
+          },
+        }, false, {})
+
+        local char = vim.fn.nr2char(vim.fn.getchar())
+
+        vim.cmd("echo ''")
+
+        if char:lower() ~= "y" then
+          return
+        end
+      end
+
+      vim.cmd("normal! m" .. mark)
+    end
+
+    for c in ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"):gmatch(".") do
+      vim.keymap.set("n", "m" .. c, function()
+        set_mark(c)
+      end, {
+        silent = true,
+        desc = "Set mark with confirmation",
+      })
+    end
+  end,
 }
